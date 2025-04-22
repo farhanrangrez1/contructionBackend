ee618461b355bf84500c964e1fde3a991c1cb83b
FarhanRangrez11 <farhanrangrez11@gmail.com>
2025-04-22 18:16:17 +0530

    TimeSheet Api

--- backend/routerapi.js
+++ backend/routerapi.js
@@ -4,8 +4,10 @@ const express = require('express');
 const routerapi = express.Router()
 
 // THEN your routers
+// Projects
 routerapi.use('/api/projects', require('./Router/projectsRouter'));
-
+// Diaries
 routerapi.use('/api/diaries', require('./Router/DiariesRouter'));
-
+// TimeSheet 
+routerapi.use('/api/timesheet', require('./Router/TimeSheetRouter'));
 module.exports = routerapi
