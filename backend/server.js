abac912853f2c00cb247a4627429e2dfa971c720
FarhanRangrez11 <farhanrangrez11@gmail.com>
2025-04-22 12:24:53 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const { config } = require('dotenv');
+const express = require('express');
+const { DBconnect } = require('./Config/db_config');
+const colors=require('colors')
+require("dotenv").config()
+
+const app =express()
+const PORT = process.env.PORT || 3000
+DBconnect()
+
+
+app.post('/',(req,res)=>{
+    res.send('Hello World')
+})
+
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`)
+})  
