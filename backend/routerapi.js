47989d0806c02d4f39b79bae74a9361e77b580b9
FarhanRangrez11 <farhanrangrez11@gmail.com>
2025-04-22 16:18:54 +0530

    chang d

--- backend/routerapi.js
+++ backend/routerapi.js
@@ -6,4 +6,6 @@ const routerapi = express.Router()
 // THEN your routers
 routerapi.use('/api/projects', require('./Router/Projects/projectsRouter'));
 
+routerapi.use('/api/diaries', require('./Router/Diaries/DiariesRouter'));
+
 module.exports = routerapi
